52e9b0ab3ca35d103dbe4e30f7fc0f0e8cb7f97a
rafael.amorim <raffaekk@gmail.com>
2026-05-18 15:43:38 -0300

    feat: Implementacao do endpoint versao_oracle
    - schema → Definicao do Schema  VersaoOracle
    
    - app → Implementacao do endpoint versao_oracle
    
    - database → inclusao do parametro ‘thick_mode’ na engine
    
    - init → Modulo init do projeto

--- app_prontocardio/app.py
+++ app_prontocardio/app.py
@@ -1,10 +1,31 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy import text
+from sqlalchemy.orm import Session
 
-app = FastAPI(tittle='API Hospital Prontocardio')
+from app_prontocardio.database import get_session_prod
+from app_prontocardio.schema import VersaoOracle
+
+app = FastAPI(title='API Hospital Prontocardio')
 
 
 @app.get('/', status_code=HTTPStatus.OK)
 def read_root():
     return {'message': 'Olá Mundo! API Hospital Prontocardio'}
+
+
+@app.get(
+    '/versao_oracle/',
+    status_code=HTTPStatus.OK,
+    response_model=list[VersaoOracle],
+)
+def versao_oracle(session: Session = Depends(get_session_prod)):
+
+    rows = session.execute(
+        text('SELECT * FROM v$version')
+    ).mappings().all()
+
+    versoes = [VersaoOracle(**row) for row in rows]
+
+    return versoes
