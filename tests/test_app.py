b22d7e96965e11771532388c5fd3132790c47a97
rafael.amorim <raffaekk@gmail.com>
2026-05-18 13:05:36 -0300

    feat: Primeiro endpoint app

---
+++ tests/test_app.py
@@ -0,0 +1,15 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from app_prontocardio.app import app
+
+
+def test_root():
+
+    cliente = TestClient(app)
+
+    response = cliente.get('/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Olá Mundo!'}
